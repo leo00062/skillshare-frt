cc3308b2147167c3d311327166d2b5afbd1ba62b
leo00062 <carpentierleo2001@gmail.com>
2025-06-05 11:12:29 +0200

    Update

---
+++ public/assets/js/navbar.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.querySelector(".navbar .toggle");
+
+  if (toggleBtn) {
+    toggleBtn.addEventListener("click", () => {
+      toggleBtn.classList.toggle("active");
+    });
+  }
+});
--- server.js
+++ server.js
@@ -6,6 +6,31 @@ import indexRoutes from "./routes/index.js";
 dotenv.config();
 // création de l'application
 const app = express();
+const API_URL = process.env.API_URL;
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "'unsafe-eval'",
+          "localhost:*",
+        ],
+        connectSrc: ["'self'", "ws://localhost:*", API_URL],
+        imgSrc: ["'self'", "data:", "blob:", API_URL],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        formAction: ["'self'"],
+        baseUri: ["'self'"],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+    crossOriginOpenerPolicy: false,
+  })
+);
+
 const __dirname__ = path.resolve();
 // initialisation du moteur de templates
 app.set("view engine", "ejs");
